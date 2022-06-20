62efad58fe02bddc1ef699df3c9642623ea3fd63
bibi941 <sssrrr@vip.qq.com>
2022-06-20 17:11:52 +0800

    添加泛型

--- test.ts
+++ test.ts
@@ -9,13 +9,16 @@ type AS = keyof Person
 
 type K2 = keyof Person[]
 
+
+ 
 interface Male<T> extends Person {
   gender: T
 }
 
 type  gender = "male" | 'famale'
-let bibi: Male<gender> = {
+ 
+let bibi: Male<gender> = { 
   age: 12,
-  name: 'bibi',
+  name: 'bibi', 
   gender: 'male'
 }
