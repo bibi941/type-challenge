8cb781ca3c6b9d5a6f5757fd2c605fbac6b52860
bibi941 <sssrrr@vip.qq.com>
2022-10-09 13:59:59 +0800

    feat: 实现MyLastString

---
+++ src/common/MyLastString.ts
@@ -0,0 +1,13 @@
+import { last, result } from "lodash"
+
+// 实现一个通用LastString<T>，它接受一个String并返回其最后一个字符
+{
+  
+  type S = 'ji ni tai mei'
+  type A = 'ikun'
+  type MyLastString<T extends string> = T extends `${infer Front}${infer Last}` ? Last extends '' ? Front : MyLastString<Last> : never
+
+  type Result = MyLastString<S>// expected to be 'i'
+  type Result2 = MyLastString<A>// expected to be 'n'
+
+}
