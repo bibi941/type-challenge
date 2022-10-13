0f89f0185d9516c4219c9a6b1f6e0bfe860be8f5
bibi941 <sssrrr@vip.qq.com>
2022-10-13 18:38:44 +0800

    feat: add MyUnion

--- src/common/MyIsNever.ts
+++ src/common/MyIsNever.ts
@@ -1,11 +1,11 @@
 {
   // Implement a type IsNever, which takes input type T. If the type of resolves to never, return true, otherwise false.
-  type IsNever<T> = [T] extends [never] ? true : false;
+  type MyIsNever<T> = [T] extends [never] ? true : false;
 
-  type A = IsNever<never>  // expected to be true
-  type B = IsNever<undefined> // expected to be false
-  type C = IsNever<null> // expected to be false
-  type D = IsNever<[]> // expected to be false
-  type E = IsNever<number> // expected to be false
+  type A = MyIsNever<never>  // expected to be true
+  type B = MyIsNever<undefined> // expected to be false
+  type C = MyIsNever<null> // expected to be false
+  type D = MyIsNever<[]> // expected to be false
+  type E = MyIsNever<number> // expected to be false
   
 }
